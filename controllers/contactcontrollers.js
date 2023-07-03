ba20ebc0d1fcd66024a697ba59bb4cd25f50ac38
hp <vinayakmalviya21@gmail.com>
2023-07-04 00:21:38 +0530

    second commit

--- controllers/contactcontrollers.js
+++ controllers/contactcontrollers.js
@@ -49,7 +49,19 @@ const getContact = asyncHandler( async (req,res) => {
 // @access public
 
 const updateContact = asyncHandler( async (req,res) => {
-    res.status(200).json({message : `Update contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new:true}
+    );
+
+    res.status(200).json(updatedContact);
 });
 
 // ------------------------------
@@ -59,7 +71,13 @@ const updateContact = asyncHandler( async (req,res) => {
 // @access public
 
 const deleteContact = asyncHandler( async (req,res) => {
-    res.status(200).json({message : `Delete contact for ${req.params.id}`});
+    const contact = await Contact.findById(req.params.id);
+    if(!contact){
+        res.status(404);
+        throw new Error("Contact not found");
+    }
+    res.status(200).json(contact);
+    await contact.deleteOne();
 });
 
 module.exports = { getContacts , createContact , getContact , updateContact , deleteContact };
--- server.js
+++ server.js
@@ -8,7 +8,9 @@ connectDb();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-app.use('/api/contacts',require('./routes/contactRoutes'));app.use(errorHandler);
+app.use('/api/contacts',require('./routes/contactRoutes'));
+app.use('/api/contacts',require('./routes/contactRoutes'));
+app.use(errorHandler);
 
 app.listen(port,() => {
     console.log(`Server running on ${port}`);
